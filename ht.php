6e48f4fd3505be3b859318cd487d78cde6837a59
Ripon Kumar Shaha <riponshaha13@gmail.com>
2024-12-19 00:15:37 +0600

    Add files via upload

---
+++ ht.php
@@ -0,0 +1,38 @@
+<html>
+<html>
+    <body style="background-color:aquamarine;">
+        <haed>
+            <title>Cricket</title>
+        </haed>
+        <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+          }
+        </style>
+        <table border="1" width="800px" align="center">
+            <tr style="background-color:DodgerBlue;">
+                <td colspan="2" align="center">
+                    <img src="https://c.ndtvimg.com/2023-12/qhgfbe4o_t20-world-cup-2024-logo_625x300_07_December_23.jpg" 
+                    width="200px" height="100px">
+                    <br>
+                    <p align="center"><b><i>  The Twenty20 International cricket tournament, organised by(ICC)</i></b></p>
+
+                </td>
+            </tr>
+            <tr>
+                <td > <img src="https://images.indianexpress.com/2023/11/ANGELO-MATHEWS-TIMED-OUT-PTI-CROP.png" width="300px" height="300px">
+                    <br>
+                    
+                <p><b>Bangladesh won by 2 wickets</b></p></td>
+                <td width="300px"><h3><u><h3>Upcoming match</h3><br></h3><br>
+                    <b>08april24:NEd vs SA</b>
+                <br><b>08april24: AUS vs ENG</b></td> 
+            </tr>
+            <tr>
+                <td colspan="2" align="center" style="background-color:DodgerBlue;"><b>For any update, pleas follow our sibsite</b></td>
+            </tr>
+
+        </table>
+    </body>
+</html>
